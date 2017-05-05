fd967e80f671a467694f1e17afbfaef989bc34c2
ulfahsf <ulfahsuryanifithri8@gmail.com>
2017-05-05 13:03:20 +0700

    c_siswa

--- application/controllers/siswa/c_siswa.php
+++ application/controllers/siswa/c_siswa.php
@@ -11,20 +11,25 @@ class C_siswa extends CI_Controller {
 
 	public function logout() {
 		$this->session->unset_userdata('username');
-		$this->session->unset_userdata('level');
 		session_destroy();
-		redirect('auth');
+		$this->load->view('index');
 	}
+	
 	public function viewNilaiSiswa(){
 		$data['user'] = $this->model_user->viewNilaiSiswa()->result();
 		$this->load->view('siswa/viewNilaiSiswa',$data);
 	}
 	public function viewJadwalSiswa(){
-		$data['user'] = $this->model_user->viewJadwalSiswa()->result();
+		$data['jadwal'] = $this->model_user->viewJadwalSiswa()->result();
 		$this->load->view('siswa/viewJadwalSiswa',$data);
 	}
-	public function formSiswa() {
-		$this->load->view('siswa/formSiswa');
+	public function loginSiswa() {
+		$this->load->view('siswa/loginSiswa');
 	}
+
+	public function viewProfile() {
+		$this->load->view('siswa/viewProfileSiswa');
+	}
+
 }
 ?>
